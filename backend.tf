72c5c06070ea32c78e120fd8d40601479fba90ba
joh887 <johminsoo@gmail.com>
2023-07-13 14:05:38 +1000

    Added logging bucket and formatted tf files.

--- backend.tf
+++ backend.tf
@@ -1,22 +1,22 @@
-provider "aws"{
-    region = "ap-southeast-2"
+provider "aws" {
+  region = "ap-southeast-2"
 }
 
 terraform {
-    required_version = ">= 1.0.0,< 2.0.0"
-    
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.0"
-        }
-    }
+  required_version = ">= 1.0.0,< 2.0.0"
 
-    backend "s3"{
-        bucket = "tf-aws-joh887-cloud-resume-challenge-state-bucket"
-        key = "terraform.tfstate"
-        region = "ap-southeast-2"
-        encrypt = true
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
+
+  backend "s3" {
+    bucket  = "tf-aws-joh887-cloud-resume-challenge-state-bucket"
+    key     = "terraform.tfstate"
+    region  = "ap-southeast-2"
+    encrypt = true
+  }
 }
 

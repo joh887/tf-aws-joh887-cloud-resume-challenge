8533b77f9c5b16de566085f1fd744b81acbcd43b
joh887 <77087931+joh887@users.noreply.github.com>
2023-07-13 13:57:11 +1000

    Revert "Testing tfsec"

--- main.tf
+++ main.tf
@@ -1,31 +1,7 @@
-#tfsec:ignore:aws-s3-enable-versioning
-#tfsec:ignore:aws-s3-encryption-customer-key
-
-resource "aws_s3_bucket" "cloud_resume_site_bucket" {
+resource "aws_s3_bucket" "b" {
   bucket = "tf-aws-joh887-cloud-resume-challenge"
-
-   server_side_encryption_configuration {
-     rule {
-        bucket_key_enabled = true
-
-        apply_server_side_encryption_by_default {
-         sse_algorithm     = "AES256"
-       }
-     }
-   }
+  acl    = "private"
 }
-
-resource "aws_s3_bucket_public_access_block" "cloud_resume_site_bucket" {
-  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
-}
-
-
-
-
 
 
 #TODO: prevent billshock in AWS

906ff0c85799e0e01b334aff9340bf635c8c0e1b
joh887 <johminsoo@gmail.com>
2023-07-13 15:12:56 +1000

    Addressed Deprecation warning and Error

--- main.tf
+++ main.tf
@@ -1,68 +1,86 @@
 #tfsec:ignore:aws-s3-enable-versioning
 #tfsec:ignore:aws-s3-encryption-customer-key
-
 resource "aws_s3_bucket" "cloud_resume_site_bucket" {
-  bucket = "tf-aws-joh887-cloud-resume-challenge"
+  bucket = "tf-aws-mgrassi-cloud-resume-challenge-site"
+}
 
-  logging {
-    target_bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
-  }
 
-  server_side_encryption_configuration {
-    rule {
-      bucket_key_enabled = true
+resource "aws_s3_bucket_server_side_encryption_configuration" "cloud_resume_site_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
 
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    bucket_key_enabled = true
+    
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "cloud_resume_site_bucket" {
-  bucket                  = aws_s3_bucket.cloud_resume_site_bucket.id
+  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-
 #tfsec:ignore:aws-s3-enable-bucket-logging
 #tfsec:ignore:aws-s3-encryption-customer-key
-
-
 resource "aws_s3_bucket" "cloud_resume_logging_bucket" {
-  bucket = "tf-aws-joh887-cloud-resume-challenge-logging"
+  bucket = "tf-aws-mgrassi-cloud-resume-challenge-logging"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "cloud_resume_logging_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      bucket_key_enabled = true
+resource "aws_s3_bucket_server_side_encryption_configuration" "cloud_resume_logging_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
 
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    bucket_key_enabled = true
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "cloud_resume_logging_bucket" {
-  bucket                  = aws_s3_bucket.cloud_resume_site_bucket.id
+  bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "cloud_resume_logging_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
 
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
+resource "aws_s3_bucket_acl" "cloud_resume_logging_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "cloud_resume_logging_bucket" {
+  bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+
+  target_bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+  target_prefix = "log/"
+}
 
 #TODO: prevent billshock in AWS
-#TODO: introduce tfsec
 #TODO: introduce best practice tf project structure
-#TODO: add state lock dynamodb
 #TODO: add default tags

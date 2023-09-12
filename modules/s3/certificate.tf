da71cf123c32cb5ec2703f6a53c52519be25e057
joh887 <johminsoo@gmail.com>
2023-09-12 16:43:37 +1000

    fix syntax error in acm_certificate

--- modules/s3/certificate.tf
+++ modules/s3/certificate.tf
@@ -1,25 +1,20 @@
-provider "aws"{
-    alias = "virginia"
-    region = "us-east-1"
-}
-
 resource "aws_acm_certificate" "m" {
     provider = aws.virginia
     domain_name = var.R53DomainName
     validation_method = "DNS"
-}
 
-subject_alternative_names = ["*.${var.R53DomainName}"]
+    subject_alternative_names = ["*.${var.R53DomainName}"]
 
-key_algorithm = "RSA_2048"
+    key_algorithm = "RSA_2048"
 
-lifecycle {
-    create_before_destroy = true
-}
+    lifecycle {
+        create_before_destroy = true
+    }
 
-  tags = {
-    Name = "Certificate for ${var.R53DomainName}.",
-    Misc = "Made with Terraform."
+    tags = {
+        Name = "Certificate for ${var.R53DomainName}.",
+        Misc = "Made with Terraform."
+    }
 }
 
 resource "aws_route53_record" "m" {

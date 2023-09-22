cfdfdaf773d4f086261a1cab08bdbc0a76f5f25f
joh887 <johminsoo@gmail.com>
2023-09-22 11:12:15 +1000

    Fetch the API endpoint instead

--- modules/api_gateway.tf
+++ modules/api_gateway.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_rest_api" "api" {
   name        = "ServerlessCount"
-  description = "Terraform maid api gateway for count user lambda function"
+  description = "Terraform made api gateway for count user lambda function"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
